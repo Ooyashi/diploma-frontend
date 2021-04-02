df7479732bfba5386efab03580c48de2b57097c8
Dmytro Kotsiuba <dmytrokocyuba@gmail.com>
2021-04-02 17:11:25 +0300

    implemented basic log and reg pages

---
+++ webapp/src/components/index.ts
@@ -0,0 +1,7 @@
+export * from './Login';
+export * from './Register';
+export * from './Home';
+
+export { default as HomeComponent } from './Home/home';
+export { default as LoginComponent } from './Login/login';
+export { default as RegisterComponent } from './Register/register';
